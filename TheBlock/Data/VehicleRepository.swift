8cc895447a39ca2c1220d99ee9be24d0b0a292c2
Hannah <fowlerhg@gmail.com>
2026-04-30 20:28:13 -0400

    Add VehicleRepository to load vehicles
    - load vehicles.json from bundle
    - decode into Vehicle models
    - handle key mapping and date parsing
    - pass errors with throws

--- TheBlock/Data/VehicleRepository.swift
+++ TheBlock/Data/VehicleRepository.swift
@@ -1,3 +1,25 @@
 import Foundation
 
-struct VehicleRepository {}
+struct VehicleRepository {
+    enum RepositoryError: Error {
+        case fileNotFound
+    }
+
+    static func loadAll() throws -> [Vehicle] {
+        guard let url = Bundle.main.url(forResource: "vehicles", withExtension: "json") else {
+            throw RepositoryError.fileNotFound
+        }
+        let data = try Data(contentsOf: url)
+        return try vehicleDecoder.decode([Vehicle].self, from: data)
+    }
+}
+
+private let vehicleDecoder: JSONDecoder = {
+    let d = JSONDecoder()
+    d.keyDecodingStrategy = .convertFromSnakeCase
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    d.dateDecodingStrategy = .formatted(formatter)
+    return d
+}()
